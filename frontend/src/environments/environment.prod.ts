a79efa8edbffba1fccfcfab60bbc4109cd73fe62
Bat0al-k <batoalkaram0@gmail.com>
2025-10-18 21:36:03 +0300

    feat: add completed frontend project (Angular 17)

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,
+    // apiUrl: 'http://localhost:3000/', // backend server in development time
+    apiUrl: 'https://fashion-platform-khaki.vercel.app/', // server in production time
+    STRIPE_PUBLIC_KEY:"pk_test_51ReehwCeRxTw56DAQP29EIc9S3qQZSON58pTvbD9LXxNfMMjQLBXBUOlm1NfI6MFkGdaHv1rPJRMiSUWufYZBjR600LOV4H32U"
+    ,firebaseConfig: {
+        apiKey: "AIzaSyDl4cTC6-LXjyXXSt1__jAeX6EQOpIHIzs",
+        authDomain: "fashion-platform-c82b9.firebaseapp.com",
+        projectId: "fashion-platform-c82b9",
+        storageBucket: "fashion-platform-c82b9.appspot.com",
+        messagingSenderId: "573354982996",
+        appId: "1:573354982996:web:e4eb4de94b1820843a37c1",
+        measurementId: "G-9EB3NP94JT"
+    }
+};
